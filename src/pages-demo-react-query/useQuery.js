ca821ce428773171b8268fcf58756b3484fc3a5a
Ivan Dalmet <ivan@dalmet.fr>
2020-03-20 17:16:30 +0100

    feat: Add update cache for cats on react-query exemple

--- src/pages-demo-react-query/useQuery.js
+++ src/pages-demo-react-query/useQuery.js
@@ -1,4 +1,5 @@
-import { useQuery } from 'react-query';
+import { useEffect } from 'react';
+import { useQuery, queryCache } from 'react-query';
 import fetcher from '../config/fetcher';
 
 export const useCat = (catId) => useQuery(
@@ -9,10 +10,20 @@ export const useCat = (catId) => useQuery(
   }
 )
 
-export const useCats = () => useQuery(
-  'images/search?limit=12&order=RANDOM',
-  fetcher(),
-  {
-    staleTime: 24 * 60 * 60 * 1000, // Prevent refetch during 24 hours
-  }
-);
+export const useCats = () => {
+  const query = useQuery(
+    'images/search?limit=12&order=RANDOM',
+    fetcher(),
+    {
+      staleTime: 24 * 60 * 60 * 1000, // Prevent refetch during 24 hours
+    }
+  );
+
+  useEffect(() => {
+    (query.data || []).forEach((cat) => {
+      queryCache.setQueryData(`images/${cat.id}`, () => cat);
+    });
+  }, [query.data]);
+
+  return query;
+};
